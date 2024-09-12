e85afbc3184da8aa6897c11dbcd6d61fc54d2a60
Erapart <malle.erapart@tktk.ee>
2024-09-12 14:58:02 +0300

    week 3

---
+++ Week3RemainderOperator/Program.cs
@@ -0,0 +1,30 @@
+﻿// Odd or even
+
+int a = 11;
+
+int result = a % 2;
+
+Console.WriteLine(result);
+
+//if(result == 0)
+//{
+//    Console.WriteLine($"{a} is an even number");
+//}
+//else
+//{
+//    Console.WriteLine($"{a} is an odd number");
+//}
+
+switch (result)
+{
+    case 0:
+        Console.WriteLine($"{a} is an even number");
+        break;
+    case 1:
+        Console.WriteLine($"{a} is an ódd number");
+        break;
+    default:
+        Console.WriteLine("Invalid value");
+        break;
+
+}
